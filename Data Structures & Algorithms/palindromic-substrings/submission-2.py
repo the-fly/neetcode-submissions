36c0295109e62f1d16e4001b3ac3cd8150336bdc
Rachit Garg <rachitgarg.rg1907@gmail.com>
2026-06-29 18:15:00 +0530

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        
+        length = len(s)
+        res = 0
+        
+        # oddLen 
+        oddOriginSet = set([a for a in range(length)])   
+        evenOriginSet = set([a for a in range(length)])     
+        span = 0
+        while oddOriginSet or evenOriginSet:
+            newOddSet = set()
+            for origin in oddOriginSet:
+                if 0<= (origin-span)  and (origin+span) < length and s[origin-span] == s[origin+span]:
+                    newOddSet.add(origin)
+            oddOriginSet = newOddSet
+            res+= len(oddOriginSet)
+
+            newEvenSet = set()
+            for origin in evenOriginSet:
+                if 0 <= (origin-span) and (origin+span+1) < length and s[origin-span] == s[origin+span+1]:
+                    newEvenSet.add(origin)
+            evenOriginSet = newEvenSet
+            res+= len(evenOriginSet)
+            
+            span += 1
+        
+        return res
+
+
+
+
